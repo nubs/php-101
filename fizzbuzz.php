8114c2393a1645bffe2fb8a67bb503219c76f541
Spencer Rinehart <anubis@overthemonkey.com>
2015-08-18 17:02:59 -0500

    :wine_glass: :bee:

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -7,3 +7,4 @@ $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
  If $input is divisable by 5 output 'Buzz'
  If $input is divisable by both 3 and 5 output 'FizzBuzz'
 */
+echo$input%3?'':Fizz,$input%5?'':Buzz;
